1b20acb60a2335574e2d6998f4b2eff0d0e1bbd4
agus <sanchezagus.1995@gmail.com>
2026-05-27 12:38:14 -0300

    Update app.js

--- frances/app.js
+++ frances/app.js
@@ -156,8 +156,8 @@ function calcular() {
 
   const cuotaSinIva = cuotaFijaSinIVA(M, n, i);
   const interes1 = interesPrimerMes(M, i);
-  const iva1 = ivaPrimerMes(interes1);
-  const cuota1 = cuotaSinIva + iva1;
+  const cargoExtra = M * 0.012;
+  const cuota1 = cuotaSinIva + iva1 + cargoExtra;
 
   const saldoPrev = saldoPrevio(M, i, n);
   const interesUlt = interesUltima(saldoPrev, i);
